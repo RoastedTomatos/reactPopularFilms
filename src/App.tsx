5df6816bdc8ea004df1e0779faab97f70d99684a
RoastedTomatos <v.honcharov.08@gmail.com>
2021-10-07 19:14:27 +0300

    master

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Header } from './components/common';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
